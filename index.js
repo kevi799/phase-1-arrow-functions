c894714ce996acef5888a92dd5d7f28d1456381c
kelvinMulinge <kelvinmulinge9702@gmail.com>
2024-09-26 14:29:04 +0300

    adds arrow function

---
+++ index.js
@@ -0,0 +1,11 @@
+// function to divide numbers
+function divide (num1, num2) {
+    return num1/num2;
+}
+console.log(divide(8,2));
+// arrow function to multiply a number by itself
+const square = (int) => Math.pow(int,2);
+console.log(square(9302));
+// arrow function to add numbers
+const add = (num1, num2) => num1 + num2;
+console.log(add(378, 282));
